858f21f2ec804a06cf5ea00467a9d8df747e48f2
Luiza Carla <luizacarla2004@gmail.com>
2024-11-29 14:02:28 -0300

    Update apagarConta.js
    Melhorando a lógica do código

--- src/main/webapp/scripts/apagarConta.js
+++ src/main/webapp/scripts/apagarConta.js
@@ -6,45 +6,42 @@ if (elementoButtonExcluirConta) {
 }
 
 function excluirConta() {
-    excluirConversa(false);
+    let isMensagensExcluidas = excluirConversa();
 
-    // Adicionando um curto período de espera para evitar problemas com o banco de dados
-    setTimeout(() => {
-        // Realizando uma requisição assíncrona ao servidor usando jQuery AJAX, permitindo excluir a conta do usuário dinamicamente sem recarregar a página
-        $.ajax({
-            method: "POST",
-            url: "ExcluirConta",
-            success: function (response) {
-                let isContaDesativada = response;
-
-                if (isContaDesativada === "true") {
-                    finalizarSessao();
-                } else {
-                    alert("Não foi possível excluir sua conta. Tente novamente mais tarde!");
+    if (isMensagensExcluidas === "true") {
+        // Adicionando um curto período de espera para evitar problemas com o banco de dados
+        setTimeout(() => {
+            // Realizando uma requisição assíncrona ao servidor usando jQuery AJAX, permitindo excluir a conta do usuário dinamicamente sem recarregar a página
+            $.ajax({
+                method: "POST",
+                url: "ExcluirConta",
+                success: function (response) {
+                    let isContaDesativada = response;
+    
+                    if (isContaDesativada === "true") {
+                        finalizarSessao();
+                    } else {
+                        alert("Não foi possível excluir sua conta. Tente novamente mais tarde!");
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.log("Erro: " + error);
                 }
-            },
-            error: function (xhr, status, error) {
-                console.log("Erro: " + error);
-            }
-        });
-    }, 100);
+            });
+        }, 100);
+    } else {
+        alert("Não foi possível excluir sua conversa. Tente novamente mais tarde!");
+    }
 }
 
-function excluirConversa(atualizarPagina) {
+function excluirConversa() {
     // Realizando uma requisição assíncrona ao servidor usando jQuery AJAX, permitindo excluir a conversa do usuário dinamicamente sem recarregar a página
     $.ajax({
         method: "POST",
         url: "ExcluirConversa",
         success: function (response) {
             let isMensagensExcluidas = response;
-
-            if (isMensagensExcluidas === "true") {
-                if (atualizarPagina === true) {
-                    window.location.href = "aiChat.jsp";
-                }
-            } else {
-                alert("Não foi possível deletar sua conversa. Tente novamente mais tarde!");
-            }
+            return isMensagensExcluidas;
         },
         error: function (xhr, status, error) {
             console.log("Erro: " + error);
