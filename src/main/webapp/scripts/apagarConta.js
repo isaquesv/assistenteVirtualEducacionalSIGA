dabc732e49922ed3f6de9eaf75494030bea200ee
Luiza Carla <luizacarla2004@gmail.com>
2024-11-29 13:55:48 -0300

    Create apagarConta.js
    Adicionando funções de excluir conta e excluir conversa

---
+++ src/main/webapp/scripts/apagarConta.js
@@ -0,0 +1,53 @@
+// Script JS presente na página apagarConta
+const elementoButtonExcluirConta = document.querySelector(".botaoSimApagarConta");
+
+if (elementoButtonExcluirConta) {
+    elementoButtonExcluirConta.addEventListener("click", excluirConta);
+}
+
+function excluirConta() {
+    excluirConversa(false);
+
+    // Adicionando um curto período de espera para evitar problemas com o banco de dados
+    setTimeout(() => {
+        // Realizando uma requisição assíncrona ao servidor usando jQuery AJAX, permitindo excluir a conta do usuário dinamicamente sem recarregar a página
+        $.ajax({
+            method: "POST",
+            url: "ExcluirConta",
+            success: function (response) {
+                let isContaDesativada = response;
+
+                if (isContaDesativada === "true") {
+                    finalizarSessao();
+                } else {
+                    alert("Não foi possível excluir sua conta. Tente novamente mais tarde!");
+                }
+            },
+            error: function (xhr, status, error) {
+                console.log("Erro: " + error);
+            }
+        });
+    }, 100);
+}
+
+function excluirConversa(atualizarPagina) {
+    // Realizando uma requisição assíncrona ao servidor usando jQuery AJAX, permitindo excluir a conversa do usuário dinamicamente sem recarregar a página
+    $.ajax({
+        method: "POST",
+        url: "ExcluirConversa",
+        success: function (response) {
+            let isMensagensExcluidas = response;
+
+            if (isMensagensExcluidas === "true") {
+                if (atualizarPagina === true) {
+                    window.location.href = "aiChat.jsp";
+                }
+            } else {
+                alert("Não foi possível deletar sua conversa. Tente novamente mais tarde!");
+            }
+        },
+        error: function (xhr, status, error) {
+            console.log("Erro: " + error);
+        }
+    });
+}
